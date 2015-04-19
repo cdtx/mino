180c4bad8b60c3e2469fec9b9651cb4f9d66c662
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2015-04-19 19:14:44 +0200

    Remove the annoying DTD url when generating graphviz svg

--- plugins/graphviz/plugin.py
+++ plugins/graphviz/plugin.py
@@ -1,5 +1,10 @@
 import pydot
+import re
 
 def run(content, output, globals=None, locals=None):
-    strSVG = pydot.graph_from_dot_data(content).create_svg()
-    locals['self'].append(globals['mdTextLine'](content=strSVG))
+    strSVG = pydot.graph_from_dot_data(content)
+
+    # Remove the annoying DTD url (not sure the best way to do this)
+    s = strSVG.create_svg()
+    s = re.sub(r'^.*?(?=<svg)', r'', s, 0, re.DOTALL|re.MULTILINE)
+    locals['self'].append(globals['mdTextLine'](content=s))
