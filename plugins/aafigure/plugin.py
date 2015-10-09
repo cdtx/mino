1538356ce549de9ad7942f7003776fa1840705ef
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2015-10-09 23:19:02 +0200

    aafigure svg plugin write the whole dimensionsin svg element

--- plugins/aafigure/plugin.py
+++ plugins/aafigure/plugin.py
@@ -5,8 +5,9 @@ def run(content, output, globals=None, locals=None):
     # http://docutils.sourceforge.net/sandbox/aafigure/README.txt
     options = {'format':'svg', 'proportional':True, 'scale':0.9}
     # SVG is better quality
-    content = render(input=unicode(content), options=options)[1].getvalue().replace(r'\n', r'\r\n')
+    rend = render(input=unicode(content), options=options)
+    content = rend[1].getvalue().replace(r'\n', r'\r\n')
 
     # Remove the annoying xml header (not sure the best way to do this)
-    content = re.sub(r'^.*?(?=<svg)', r'', content, 0, re.DOTALL|re.MULTILINE)
+    content = re.sub(r'^.*?<svg', r'<svg %s' % rend[0].get_size_attrs(), content, 0, re.DOTALL|re.MULTILINE)
     locals['self'].append(globals['mdTextLine'](content=content, inline=False))
