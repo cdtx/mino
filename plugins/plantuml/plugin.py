ffd2562d00573505835bed94d06ecf5fdfbe2cbb
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2015-10-11 23:57:07 +0200

    Changed how plantuml plugin works : include inline svg in the document

--- plugins/plantuml/plugin.py
+++ plugins/plantuml/plugin.py
@@ -1,8 +1,14 @@
 import re
 import plantuml
+import urllib
 
 def run(content, output, globals=None, locals=None):
     url = plantuml.PlantUML().get_url(content)
     # SVG is better quality
-    url = re.sub(r'http://www.plantuml.com/plantuml/img/', r'http://www.plantuml.com/plantuml/png/', url)
-    locals['self'].append(globals['mdImage'](url=url))
+    url = re.sub(r'http://www.plantuml.com/plantuml/img/', r'http://www.plantuml.com/plantuml/svg/', url)
+    # Get the content at this url
+    content = urllib.urlopen(url).read()
+
+    # Remove the annoying xml header (not sure the best way to do this)
+    content = re.sub(r'^.*?<svg', r'<svg', content, 0, re.DOTALL|re.MULTILINE)
+    locals['self'].append(globals['mdTextLine'](content=content, inline=False))
