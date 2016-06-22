4147fdb62e93ec1f46cbf850501bab0e1a2771f0
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2016-06-22 15:20:48 +0200

    Orgainze stuffs setuptools proof

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mino',
+    version='0.0.1',
+    description='Custom markdown language',
+    author='cdtx',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Editors',
+    ],
+    packages=['cdtx.mino'],
+)
+
+
